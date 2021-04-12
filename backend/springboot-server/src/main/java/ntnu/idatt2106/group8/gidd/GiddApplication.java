28a708aea7c8ba10e57ddd34cf995b2816a6be41
Endré Hadzalic <endrehad@stud.ntnu.no>
2021-04-12 11:08:34 +0200

    Init

---
+++ backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/GiddApplication.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2106.group8.gidd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiddApplication.class, args);
+	}
+
+}
---
+++ backend/springboot-server/src/test/java/ntnu/idatt2106/group8/gidd/GiddApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2106.group8.gidd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
