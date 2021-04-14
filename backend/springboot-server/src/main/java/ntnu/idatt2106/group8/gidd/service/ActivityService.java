4bb17472ef4df800df5432971baa49b1cd55982e
odinkvarving <odinkvarving@users.noreply.github.com>
2021-04-14 14:49:37 +0200

    Added frame for CrudRepository in Service and Dao

--- backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/service/ActivityService.java
+++ backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/service/ActivityService.java
@@ -1,13 +1,11 @@
 package ntnu.idatt2106.group8.gidd.service;
 
-import ntnu.idatt2106.group8.gidd.model.entities.Activity;
+import ntnu.idatt2106.group8.gidd.model.Activity;
 import ntnu.idatt2106.group8.gidd.repository.ActivityRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ActivityService {
@@ -16,30 +14,7 @@ public class ActivityService {
     private ActivityRepo activityRepo;
 
     public List<Activity> getAllActivities() {
-        List<Activity> activities = new ArrayList<>();
-        activityRepo.findAll().forEach(activities::add);
-        return activities;
-    }
-
-    public Optional<Activity> getActivity(int id) {
-        return activityRepo.findById(id);
-    }
-
-    public Activity addActivity(Activity activity) {
-        activityRepo.save(activity);
-        return activity;
-    }
-
-    public Activity updateActivity(int id, Activity activity) {
-        return activityRepo.save(activity);
-    }
-
-    public Activity cancelActivity(int id, Activity activity) {
-        return activityRepo.save(activity);
-    }
-
-    public void deleteActivity(int id) {
-        activityRepo.deleteById(id);
+        activityRepo.findAll();
     }
 
 }
