794b0947c34f94289a86c6a01fd3a6efa9da25ea
scottrs <scottrs@stud.ntnu.no>
2021-04-14 12:45:04 +0200

    Modified ActivityController

--- backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/controller/ActivityController.java
+++ backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/controller/ActivityController.java
@@ -10,5 +10,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/activities")
 public class ActivityController {
+    @Autowired
+    private ActivityService activityService;
 
+    @GetMapping
+    public List<Activity> getActivities() {
+        return activityService.getActivities();
+    }
+
+    @GetMapping("/{id}")
+    public Activity getActivity(@PathVariable("id") int id) {
+        return activityService.getActivity(id);
+    }
+
+    @PostMapping
+    public Activity addActivity(@RequestBody Activity activity) {
+        return activityService.addActivity(activity);
+    }
 }
