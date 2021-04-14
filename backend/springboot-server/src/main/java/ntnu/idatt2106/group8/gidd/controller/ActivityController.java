1e75b97ea2b1d7f35b1f4888b16126980f0902d3
odinkvarving <odinkvarving@users.noreply.github.com>
2021-04-14 15:39:34 +0200

    Added standard methods to Controller, Service and Repo

--- backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/controller/ActivityController.java
+++ backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/controller/ActivityController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/activities")
@@ -15,16 +16,28 @@ public class ActivityController {
 
     @GetMapping
     public List<Activity> getActivities() {
-        return activityService.getActivities();
+        return activityService.getAllActivities();
     }
 
     @GetMapping("/{id}")
-    public Activity getActivity(@PathVariable("id") int id) {
+    public Optional<Activity> getActivity(@PathVariable("id") int id) {
         return activityService.getActivity(id);
     }
 
     @PostMapping
     public Activity addActivity(@RequestBody Activity activity) {
         return activityService.addActivity(activity);
-    }*/
+    }
+
+    @PutMapping("/{id}")
+    public Activity updateActivity(@RequestParam Activity newActivity, @PathVariable("id") int id) {
+        return activityService.updateActivity(id, newActivity);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deleteActivity(@PathVariable("id") int id) {
+        activityService.deleteActivity(id);
+    }
+
 }
