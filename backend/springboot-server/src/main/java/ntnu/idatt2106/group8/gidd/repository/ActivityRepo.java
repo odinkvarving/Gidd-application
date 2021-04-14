356c4c0128597bfab28dc3b4953eacfc4bb26089
odinkvarving <odinkvarving@users.noreply.github.com>
2021-04-14 15:39:34 +0200

    Added standard methods to Controller, Service and Repo

--- backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/repository/ActivityRepo.java
+++ backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/repository/ActivityRepo.java
@@ -1,9 +1,10 @@
 package ntnu.idatt2106.group8.gidd.repository;
 
-import ntnu.idatt2106.group8.gidd.model.Activity;
+import ntnu.idatt2106.group8.gidd.model.entities.Activity;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ActivityRepo extends CrudRepository<Activity, Integer> {
+public interface ActivityRepo extends CrudRepository<Activity, Integer> {
 
 }
