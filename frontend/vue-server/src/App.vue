935dd8e7c9eb11caf20c31bc8cb409b14715ab3f
scottrs <scottrs@stud.ntnu.no>
2021-04-14 16:05:52 +0200

    Started on frontend for activity feed

--- frontend/vue-server/src/App.vue
+++ frontend/vue-server/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <ActivityCard></ActivityCard>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ActivityCard from './components/activityCardComponents/ActivityCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ActivityCard
   }
 }
 </script>
---
+++ frontend/vue-server/src/components/activityCardComponents/Info.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="container">
+        <h1>{{ name }}</h1>
+        <p>{{ description }}</p>
+        <h3>Informasjon:</h3>
+        <ul>
+            <li>Kategori:</li>
+            <li>Sted:</li>
+            <li>Tid:</li>
+            <li>Varighet:</li>
+            <li>Værmelding:</li>
+            <li>Deltakere:</li>
+        </ul>
+        <ul>
+            <li>{{ type }}</li>
+            <li>{{ place }}</li>
+            <li>{{ time }}</li>
+            <li>{{ duration }}</li>
+            <li>{{ weather }}</li>
+            <li>{{ participants }}</li>
+        </ul>
+        <button>{{ status }}</button>
+    </div>
+</template>
+<script>
+    export default {
+        name: "Info",
+
+        props: {
+            activity: null,
+        },
+
+        data() {
+            return {
+                name: "",
+                description: "",
+                type: "",
+                place: "",
+                time: "",
+                duration: "",
+                weather: "",
+                participants: "",
+                status: "",
+            }
+        },
+
+        methods: {
+            load() {
+                
+            }
+        }
+    }
+</script>
