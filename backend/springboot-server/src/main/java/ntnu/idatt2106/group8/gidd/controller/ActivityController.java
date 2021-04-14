12a90645980bc105248825e807dd38259e8ea5ee
odinkvarving <odinkvarving@users.noreply.github.com>
2021-04-14 15:39:34 +0200

    Added standard methods to Controller, Service and Repo

--- backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/controller/ActivityController.java
+++ backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/controller/ActivityController.java
@@ -3,6 +3,9 @@ package ntnu.idatt2106.group8.gidd.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/activities")
 public class ActivityController {
@@ -11,16 +14,28 @@ public class ActivityController {
 
     @GetMapping
     public List<Activity> getActivities() {
-        return activityService.getActivities();
+        return activityService.getAllActivities();
     }
 
     @GetMapping("/{id}")
-    public Activity getActivity(@PathVariable("id") int id) {
+    public Optional<Activity> getActivity(@PathVariable("id") int id) {
         return activityService.getActivity(id);
     }
 
     @PostMapping
     public Activity addActivity(@RequestBody Activity activity) {
         return activityService.addActivity(activity);
-    }*/
+    }
+
+    @PutMapping("/{id}")
+    public Activity updateActivity(@RequestParam Activity newActivity, @PathVariable("id") int id) {
+        return activityService.updateActivity(id, newActivity);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deleteActivity(@PathVariable("id") int id) {
+        activityService.deleteActivity(id);
+    }
+
 }
--- backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/service/ActivityService.java
+++ backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/service/ActivityService.java
@@ -1,11 +1,13 @@
 package ntnu.idatt2106.group8.gidd.service;
 
-import ntnu.idatt2106.group8.gidd.model.Activity;
+import ntnu.idatt2106.group8.gidd.model.entities.Activity;
 import ntnu.idatt2106.group8.gidd.repository.ActivityRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ActivityService {
@@ -14,7 +16,26 @@ public class ActivityService {
     private ActivityRepo activityRepo;
 
     public List<Activity> getAllActivities() {
-        activityRepo.findAll();
+        List<Activity> activities = new ArrayList<>();
+        activityRepo.findAll().forEach(activities::add);
+        return activities;
+    }
+
+    public Optional<Activity> getActivity(int id) {
+        return activityRepo.findById(id);
+    }
+
+    public Activity addActivity(Activity activity) {
+        activityRepo.save(activity);
+        return activity;
+    }
+
+    public Activity updateActivity(int id, Activity activity) {
+        return activityRepo.save(activity);
+    }
+
+    public void deleteActivity(int id) {
+        activityRepo.deleteById(id);
     }
 
 }
