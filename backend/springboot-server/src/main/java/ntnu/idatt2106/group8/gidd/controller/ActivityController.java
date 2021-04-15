2b63b7e8df4c864fbe387ae05f2f30648a8fcb0a
odinkvarving <odinkvarving@users.noreply.github.com>
2021-04-15 12:48:03 +0200

    Removed comment

--- backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/controller/ActivityController.java
+++ backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/controller/ActivityController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/activities")
 public class ActivityController {
-   /* @Autowired
+    @Autowired
     private ActivityService activityService;
 
     @GetMapping
