b438a049172e8be2260fff19352262c89a44901f
odinkvarving <odinkvarving@users.noreply.github.com>
2021-04-15 10:22:50 +0200

    Added test for ActivityController and logger to the DAO

--- backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/service/ActivityService.java
+++ backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/service/ActivityService.java
@@ -2,7 +2,10 @@ package ntnu.idatt2106.group8.gidd.service;
 
 import ntnu.idatt2106.group8.gidd.model.entities.Activity;
 import ntnu.idatt2106.group8.gidd.repository.ActivityRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,30 +15,53 @@ import java.util.Optional;
 @Service
 public class ActivityService {
 
+    private static final Logger log = LoggerFactory.getLogger(ActivityService.class);
+
     @Autowired
     private ActivityRepo activityRepo;
 
     public List<Activity> getAllActivities() {
         List<Activity> activities = new ArrayList<>();
-        activityRepo.findAll().forEach(activities::add);
+        try {
+            activityRepo.findAll().forEach(activities::add);
+        }catch (DataAccessException e) {
+            log.info("Activities not found");
+        }
         return activities;
     }
 
     public Optional<Activity> getActivity(int id) {
-        return activityRepo.findById(id);
+        try {
+            return activityRepo.findById(id);
+        }catch (DataAccessException e) {
+            log.info("Activity not found");
+        }
+        return Optional.empty();
     }
 
     public Activity addActivity(Activity activity) {
-        activityRepo.save(activity);
-        return activity;
+        try {
+            return activityRepo.save(activity);
+        }catch (DataAccessException e) {
+            log.info("Could not add activity");
+        }
+        return null;
     }
 
     public Activity updateActivity(int id, Activity activity) {
-        return activityRepo.save(activity);
+        try {
+            return activityRepo.save(activity);
+        }catch (DataAccessException e) {
+            log.info("Could not update activity");
+        }
+        return null;
     }
 
     public void deleteActivity(int id) {
-        activityRepo.deleteById(id);
+        try {
+            activityRepo.deleteById(id);
+        }catch (DataAccessException e) {
+            log.info("Could not delete activity");
+        }
     }
-
 }
