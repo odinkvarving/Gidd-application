8b40bec3029d5eef2a513c3ecc201340af525b77
odinkvarving <odinkvarving@users.noreply.github.com>
2021-04-15 08:35:41 +0200

    Added ActivityType files

---
+++ backend/springboot-server/src/main/java/ntnu/idatt2106/group8/gidd/controller/ActivityTypeController.java
@@ -0,0 +1,10 @@
+package ntnu.idatt2106.group8.gidd.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/activityType")
+public class ActivityTypeController {
+}
