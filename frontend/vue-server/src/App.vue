c0e2df421e3636d188c3ed069edf27b916a77117
mattiaseggen <mattiaae@stud.ntnu.no>
2021-04-15 09:33:11 +0200

    Added index.vue etc

--- frontend/vue-server/src/App.vue
+++ frontend/vue-server/src/App.vue
@@ -1,36 +1,18 @@
 <template>
-  <!--<img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-  <!--<ActivityCard></ActivityCard>-->
-  <ActivityFeed></ActivityFeed>
+  <index/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import ActivityCard from './components/activityCardComponents/ActivityCard.vue'
-import ActivityFeed from './components/activityFeedComponents/ActivityFeed.vue'
+import index from './router/index.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    ActivityCard,
-    ActivityFeed
+    index
   }
 }
 </script>
 
 <style>
 
-@import url('https://fonts.googleapis.com/css2?family=Mulish&display=swap');
-
-#app {
-  font-family: 'Mulish', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 0;
-  padding: 0;
-}
 </style>
