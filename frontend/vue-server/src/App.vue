a0c2926a40424c60be87e83ab7cb84ccde1b8a6d
mattiaseggen <mattiaae@stud.ntnu.no>
2021-04-15 09:33:11 +0200

    Added index.vue etc

--- frontend/vue-server/src/App.vue
+++ frontend/vue-server/src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <index/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import index from './router/index.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    index
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
