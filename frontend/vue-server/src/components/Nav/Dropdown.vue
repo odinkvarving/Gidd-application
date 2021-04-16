22f4fe293d26ae8b3393066551a1a052891e42f5
mattiaseggen <mattiaae@stud.ntnu.no>
2021-04-16 12:58:27 +0200

    Started on dropdown functionality for navbar

---
+++ frontend/vue-server/src/components/Nav/Dropdown.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="menu-item" @click="isVisible = !isVisible">
+      <a href="#">
+          {{ title }}
+      </a>
+        <transition name="fade" appear>
+            <div class="sub-menu" v-if="isVisible">
+                <div v-for="(item, i) in items" :key="i" class="menu-item">
+                    <router-link :to="item.link">{{ item.title }}</router-link>
+                </div>
+            </div>
+      </transition>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'dropdown',
+    props: ['title', 'items'],
+    data() {
+        return {
+            isVisible: false
+        }
+    }
+}
+</script>
+
+<style>
+nav .menu-item .sub-menu {
+    position: absolute;
+    top: calc(100% + 5px);
+    left: 50%;
+    transform: translateX(-60%);
+    width: max-content;
+    border: 1px solid black;
+}
+</style>
--- frontend/vue-server/src/components/Nav/NavBar.vue
+++ frontend/vue-server/src/components/Nav/NavBar.vue
@@ -1,30 +1,63 @@
 <template>
-  <nav>
-    <div class="menu-item"><a href="#"></a>Søk</div>
-    <div class="menu-item"><a href="#"></a>Opprett Aktivitet</div>
-    <div class="menu-item"><a href="#"></a>Varsler</div>
-    <div class="menu-item"><a href="#"></a>Bruker</div>
+  <nav class="nav-bar">
+    <div class="menu-item"><a href="#">Søk</a></div>
+    <div class="menu-item"><a href="#">Opprett Aktivitet</a></div>
+    <Dropdown title="Varslinger" :items="notifications" />
+    <Dropdown title="Bruker" :items="user" />
   </nav>
 </template>
 
 <script>
+import Dropdown from "./Dropdown.vue";
 export default {
   name: "navbar",
+  components: {
+    Dropdown,
+  },
   data() {
-    return {};
+    return {
+      user: [
+        {
+          title: "Min Profil",
+          link: "#",
+        },
+        {
+          title: "Innstillinger",
+          link: "#",
+        },
+        {
+          title: "Logg Ut",
+          link: "#",
+        },
+      ],
+      notifications: [{}],
+    };
   },
 };
 </script>
 
 <style>
-nav {
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
+.nav-bar {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 }
 
 nav .menu-item {
-    
+  color: #000;
+  padding: 10px 20px;
+  position: relative;
+  text-align: center;
+  display: flex;
 }
 
+nav .menu-item:active,
+nav .menu-item:hover {
+  background-color: rgb(219, 219, 219);
+}
+
+nav .menu-item a {
+  color: inherit;
+  text-decoration: none;
+}
 </style>
