fc10ee4b914fa7ed650d405183267c72599cbebe
Mattias Eggen <mattias.a.eggen@gmail.com>
2021-04-19 14:34:11 +0200

    fixed merge conflict in main.js

--- frontend/vue-client-app/src/main.js
+++ frontend/vue-client-app/src/main.js
@@ -11,17 +11,6 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
 Vue.use(VueRouter)
 
 const router = new VueRouter({
