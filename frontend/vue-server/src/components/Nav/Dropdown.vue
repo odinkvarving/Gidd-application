7b60ae004ab61cfd80b96ebcb132881e37e03a33
mattiaseggen <mattiaae@stud.ntnu.no>
2021-04-19 09:00:30 +0200

    progressed on navbar

--- frontend/vue-server/src/components/Nav/Dropdown.vue
+++ frontend/vue-server/src/components/Nav/Dropdown.vue
@@ -1,37 +1,35 @@
 <template>
   <div class="menu-item" @click="isVisible = !isVisible">
-      <a href="#">
-          {{ title }}
-      </a>
-        <transition name="fade" appear>
-            <div class="sub-menu" v-if="isVisible">
-                <div v-for="(item, i) in items" :key="i" class="menu-item">
-                    <router-link :to="item.link">{{ item.title }}</router-link>
-                </div>
-            </div>
-      </transition>
+    <a xlink:href="#">
+      {{ icon }}
+    </a>
+    <div class="sub-menu" v-if="isVisible">
+      <div v-for="(item, i) in items" :key="i" class="menu-item">
+        <router-link :to="item.link">{{ item.title }}</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'dropdown',
-    props: ['title', 'items'],
-    data() {
-        return {
-            isVisible: false
-        }
-    }
-}
+  name: "dropdown",
+  props: ["icon", "items"],
+  data() {
+    return {
+      isVisible: false,
+    };
+  },
+};
 </script>
 
 <style>
 nav .menu-item .sub-menu {
-    position: absolute;
-    top: calc(100% + 5px);
-    left: 50%;
-    transform: translateX(-60%);
-    width: max-content;
-    border: 1px solid black;
+  position: absolute;
+  top: calc(100% + 5px);
+  left: 50%;
+  transform: translateX(-60%);
+  width: max-content;
+  border: 1px solid black;
 }
 </style>
