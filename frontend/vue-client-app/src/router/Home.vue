2f37f849b892179d5d18e79492e579fef55455e5
Mattias Eggen <mattias.a.eggen@gmail.com>
2021-04-20 10:51:46 +0200

    added test text to front page

--- frontend/vue-client-app/src/router/Home.vue
+++ frontend/vue-client-app/src/router/Home.vue
@@ -8,7 +8,7 @@
                 <router-link to="/register"><button class="btn register"><span>Registrer deg</span></button></router-link>
             </div>
         </div>
-        <div class="test"></div>
+        <div class="info-layer">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
     </div>
 </template>
 
@@ -25,9 +25,7 @@ export default {
 
 <style scoped>
 
-
-
-.test {
+.info-layer {
     height: 1000px;
 }
 
