0be5805f3e87e0c5299e9c2346d5daa12589837c
Endré Hadzalic <endrehad@stud.ntnu.no>
2021-04-21 15:22:15 +0200

    Began with frontend part of forgot password

---
+++ frontend/vue-client-app/src/components/ForgotPasswordComponents/ForgotPasswordBox.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="input-box">
+    <router-link to="/"
+      ><p class="close-btn-p">
+        <b-icon class="text-muted" id="close-btn" icon="x"></b-icon></p
+    ></router-link>
+    <h2>Glemt passord?</h2>
+    <input v-model="emailValue" />
+    <button v-on:click="postAddress()">submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ForgotPasswordBox",
+  data() {
+    return {
+      emailValue: "",
+    };
+  },
+  methods: {
+    postAddress: function () {
+      alert("submitted");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  margin-top: 50px;
+}
+
+.input-box {
+  position: relative;
+  width: 510px;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+  background: white;
+}
+
+.or-text {
+  font-size: 13px;
+  opacity: 75%;
+  margin-top: 17px;
+}
+
+.input-container {
+  font-family: "Mulish";
+  margin: 10px 0;
+  display: flex;
+  flex-flow: column;
+}
+
+.input-container p {
+  margin: 0 0 4px 0;
+  font-size: 13px;
+  align-self: flex-start;
+  color: red;
+}
+
+.input {
+  width: 372px;
+  height: 50px;
+  padding: 0 10px;
+  background-color: #f6f6f6;
+  border: none;
+  outline: none;
+}
+
+#Login-btn {
+  margin-top: 20px;
+  font-family: "Mulish";
+  font-size: 20px;
+  width: 250px;
+  height: 42px;
+  color: white;
+  background: #ffbd3e;
+  border: none;
+  border-radius: 6px;
+  outline: none;
+}
+
+#Login-btn:hover {
+  background-color: #efb03a;
+  cursor: pointer;
+}
+
+#close-btn {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+}
+
+.close-btn-p {
+  font-size: 50px;
+}
+
+.register-text {
+  opacity: 75%;
+  margin-top: 15px;
+  font-size: 14px;
+  margin-bottom: 20px;
+}
+
+.register-text:hover {
+  cursor: pointer;
+  text-decoration: underline;
+}
+
+.invalid-login {
+  width: 75%;
+  height: 50px;
+  background-color: #ffdbdf;
+  border: solid 1px #b7666a;
+  margin: 20px 0 40px 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.invalid-login .invalid-login-text {
+  color: #91595c;
+  font-size: 13px;
+  margin: 0 0 0 20px;
+}
+
+.x-icon {
+  font-size: 20px;
+  color: #492728;
+  margin: 0 10px 0 0;
+}
+
+.x-icon:hover {
+  cursor: pointer;
+}
+
+.spacing {
+  margin-bottom: 40px;
+}
+</style>
